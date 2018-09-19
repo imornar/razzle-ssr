5035aab756a145704e69c6c105a5346006654566
imornar <ivan.mornar@toptal.com>
2018-09-19 12:18:09 +0200

    adding razzle base

---
+++ src/home/home.js
@@ -0,0 +1,44 @@
+import { withStyles, Button } from '@material-ui/core';
+import React from 'react';
+import logo from '../react.svg';
+import './Home.css';
+import homeStyles from './home.styles';
+
+function decorator() {
+  console.log('****** decorators working ******');
+}
+
+@decorator
+@withStyles(homeStyles)
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="Home-header">
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h2>Welcome to Razzle</h2>
+        </div>
+        <p className="Home-intro">
+          To get started, edit <code>src/App.js</code> or{' '}
+          <code className={this.props.classes.bgPurple}>src/Home.js</code> and save to reload.
+        </p>
+        <ul className="Home-resources">
+          <Button variant="contained" color="primary">
+            Material ui button
+          </Button>
+          <li>
+            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
+          </li>
+          <li>
+            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
+          </li>
+          <li>
+            <a href="https://palmer.chat">Community Slack</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/home/home.styles.js
@@ -0,0 +1,7 @@
+const homeStyles = theme => ({
+  bgPurple: {
+    background: 'purple'
+  }
+});
+
+export default homeStyles;
